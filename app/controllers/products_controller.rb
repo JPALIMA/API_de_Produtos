9f07f844f9204c71eb780ee6d45afb53310cbc60
Joao Pedro Araujo de Lima <jopearaujo94@gmail.com>
2023-09-11 22:02:49 -0300

    exemplo de uma API para gerenciar produtos em um catálogo.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+#app/controllers/products_controller.rb
+
+class ProductsController < ApplicationController
+     def index
+          products = Product.all
+          render json: products
+     end
+
+     def show
+          product = Product.find(params[:id])
+          render json: product
+     end
+end
